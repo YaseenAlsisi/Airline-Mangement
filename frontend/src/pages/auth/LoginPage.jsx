77707647537a6e35989f48168b2ecfe1c50c10f7
MohamedAbdelmegeed <Mohamed.Abdelhamid.1240291@CIT.hnu.edu.eg>
2026-09-03 11:29:47 +0300

    fixing some loging issues

--- frontend/src/pages/auth/LoginPage.jsx
+++ frontend/src/pages/auth/LoginPage.jsx
@@ -13,8 +13,8 @@ export const LoginPage = () => {
   const location = useLocation();
   const { t } = useTranslation();
   const { user, setTokens, setUser } = useAuthStore();
-  const [username, setUsername] = useState('admin');
-  const [password, setPassword] = useState('admin');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
 
